776c5bbce7fff89b1d77c9e5ff857ebd0048b6bf
Анна <annarainchik@gmail.com>
2022-06-24 19:35:49 +0300

    chancge homework1

--- homework.py
+++ homework.py
@@ -6,27 +6,24 @@
 def func(arg1, arg2, arg3="third"):
     print(arg1, arg2, arg3)
 
-func("hello!", arg2=123, arg3="321")
 
 
-def open_browser():
+def open_browser(browser,version,arg3="100"):
     pass
 #func("тест1", "тест2", "тест3")
 
-def go_to_companyname_homepage():
+def go_to_companyname_homepage(open,page,company):
     pass
 
 #func (arg1="привет", arg2 = "название", arg3= "компании")
 
-def find_registration_button_on_login_page():
+def find_registration_button_on_login_page(click,button,login):
     pass
 
 #func("проверка", "связи", "дз4")
 
 
-func (open_browser(),go_to_companyname_homepage(),find_registration_button_on_login_page())
-
 l = (open_browser,go_to_companyname_homepage,find_registration_button_on_login_page)
-for func.__name__ in l:
-  string =  func.__name__
-    print(string)
+
+for s in l:
+    print(s)
